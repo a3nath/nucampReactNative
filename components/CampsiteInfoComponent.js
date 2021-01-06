f5cff95a1026ecb9f02c2eefc5b00f103aba46d6
a3nath <amarn.sks@gmail.com>
2021-01-06 18:26:13 -0500

    W2 icons

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
-import {Card} from 'react-native-elements';
+import {Card, Icon} from 'react-native-elements';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import {CAMPSITES} from '../shared/campsites';
+import {COMMENTS} from '../shared/comments';
 
-function RenderCampsite({campsite}){
+function RenderCampsite(props){
+
+    const {campsite} = props
+
     if(campsite){
         return(
             <Card
@@ -12,6 +17,15 @@ function RenderCampsite({campsite}){
                      <Text style= {{margin:10}}>
                          {campsite.description}
                      </Text>
+                <Icon
+                    name={props.favourite? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    onPress={
+                        () => props.favourite ? "" : props.makeFavourite()    
+                    }
+                />
                 </Card>
         )
     }
@@ -19,13 +33,41 @@ function RenderCampsite({campsite}){
         <View/>;
 }
 
+function RenderComments({comments}){
+
+    const RenderCommentItem = ({item}) => {
+        return(
+            <View style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.text}</Text>
+                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
+                <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
+            </View>
+        )
+    }
+
+    return(
+        <Card title='Comments'>
+            <FlatList
+                data={comments}
+                keyExtractor={item => item.id.toString()}
+                renderItem={RenderCommentItem}/>
+        </Card>
+    )
+}
+
 class CampsiteInfo extends Component{
 
     constructor(props){
         super(props);
         this.state = {
-            campsites:CAMPSITES
+            campsites:CAMPSITES,
+            comments: COMMENTS,
+            favourite:false
         };
+    }
+
+    makeFavourite() {
+        this.setState({favourite : true})
     }
 
     //title for screen
@@ -37,7 +79,17 @@ class CampsiteInfo extends Component{
         // using the navigation props to extract the campsiteId that was passed in directory
         const campsiteId = this.props.navigation.getParam('campsiteId');
         const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
-        return <RenderCampsite campsite={campsite} />
+        const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId)
+        return (
+         <ScrollView>
+             <RenderCampsite 
+                campsite={campsite} 
+                favourite={this.state.favourite}
+                makeFavourite={() => this.makeFavourite()}         
+             />
+             <RenderComments comments={comments}/>
+         </ScrollView>
+        )
     }
 
 }
